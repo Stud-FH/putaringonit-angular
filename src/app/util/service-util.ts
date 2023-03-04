93db7ab6fd6dd37b7e2ec6497778c7117e56d782
flh <Florian.Herzog@ti8m.ch>
2023-03-04 18:10:18 +0100

    http options: + cors header

--- src/app/util/service-util.ts
+++ src/app/util/service-util.ts
@@ -1,22 +1,30 @@
 import {Profile} from "../domain/profile/profile";
+import {HttpHeaders} from "@angular/common/http";
 
-export function tokenOptions(): {params: {[key: string]: any}} {
+const headers = new HttpHeaders()
+  .set('content-type', 'application/json')
+  .set('Access-Control-Allow-Origin', '*');
+
+export function tokenOptions(): {params: {[key: string]: any}, headers: HttpHeaders} {
   const token = localStorage.getItem('token') ?? '';
   return {
+    headers,
     params: {token}
   };
 }
 
-export function authOptions(profile: Profile): {params: {[key: string]: any}} {
+export function authOptions(profile: Profile): {params: {[key: string]: any}, headers: HttpHeaders} {
   const token = localStorage.getItem('token') ?? '';
   return {
-    params: {token, profile: profile.identifier}
+    headers,
+    params: {token, profile: profile.identifier},
   };
 }
 
-export function updateOptions(updates: string[], profile: Profile): {params: {[key: string]: any}} {
+export function updateOptions(updates: string[], profile: Profile): {params: {[key: string]: any}, headers: HttpHeaders} {
   const token = localStorage.getItem('token') ?? '';
   return {
+    headers,
     params: {updates, token, profile: profile.identifier}
   };
 }
